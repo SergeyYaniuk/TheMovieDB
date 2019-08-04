d18c038b75dddc6e4e5fb95cf8822f7f1d1dcdaa
Sergey <sergeyyaniuk@gmail.com>
2019-08-04 16:36:48 +0300

    Set API key

--- app/src/main/java/com/sergeyyaniuk/themoviedb/utils/Constants.java
+++ app/src/main/java/com/sergeyyaniuk/themoviedb/utils/Constants.java
@@ -3,7 +3,7 @@ package com.sergeyyaniuk.themoviedb.utils;
 public final class Constants {
 
     public static final String DB_NAME = "the_movie_db.db";
-    public static final String API_KEY = "db54322ce16cf6fb1bb40cf1e61dd41d";
+    public static final String API_KEY = "api_key";
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
     public static final String BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w500/";
 
